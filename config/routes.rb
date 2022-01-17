e95827346c9342ea8b8d35f1acf587114a78d2a5
long <longlongsf@gmail.com>
2022-01-16 18:11:23 -0800

    added routes, view, controller for users

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get "about", to: "defaults#about", as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :articles
+  get "signup", to: 'users#new'
+  resources :users, except: [:new]
 end
